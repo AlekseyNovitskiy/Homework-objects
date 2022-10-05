7b1714928328756df8559110ca9e366746f9e29a
AlekseyNovitskiy <novickii_aleksei@mail.ru>
2022-10-05 16:15:08 +0300

    ДЗ методы объектов

--- src/Author.java
+++ src/Author.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Author {
 
     private String firstName;
@@ -14,4 +16,22 @@ public class Author {
     public String getSecondName() {
         return (this.secondName);
     }
+
+    public String toString() {
+        return this.firstName + " "+ this.secondName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return firstName.equals(author.firstName) && secondName.equals(author.secondName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, secondName);
+    }
 }
+
--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
 
     private String name;
@@ -23,5 +25,21 @@ public class Book {
     }
     public void setYear (int year){
         this.year = year;
+    }
+    public String toString() {
+        return "Название книги: " + this.name +" год выхода: "+ this.year + " Автор: "+this.author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && name.equals(book.name) && author.equals(book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, author, year);
     }
 }
